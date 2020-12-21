19003b54a6ed294256423dbb787b3b46afd8e294
KrishnaSiddhartha <43221324+KrishnaSiddhartha@users.noreply.github.com>
2020-12-21 02:49:02 -0600

    Update JobSearch.jsx

--- src/scenes/jobSearch/JobSearch.jsx
+++ src/scenes/jobSearch/JobSearch.jsx
@@ -27,12 +27,12 @@ export const JobSearch = () => {
             <div className='JobSearch-header'>
                 <p className='JobSearch-title'> devJobs </p>
                 <div className='JobSearch-toggle'>
-                    <img className='sun' src={sun}/>
+                    <img className='sun' alt="light theme" src={sun}/>
                     <Toggle
                         defaultChecked={false}
                         className='JobSearch-darkTheme-checkbox'
                         onChange={onClickTheme}/>
-                    <img className='moon' src={moon}/>
+                    <img className='moon' alt="dark theme" src={moon}/>
                 </div>
                 <themeContext.Provider value={darkTheme ? "dark" : "light"}>
                     <Router>
